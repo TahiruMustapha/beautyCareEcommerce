314b3a7cf29dd8d68f2e3baf5780677592554fcb
TahiruMustapha <tahirumustapha13@gmail.com>
2023-07-05 21:48:41 +0000

    add files

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ import pay from "../country/pay.png";
 import {BsFacebook, BsGoogle, BsInstagram, BsTwitter, BsYoutube} from 'react-icons/bs'
 const Footer = () => {
   return (
-    <div className="mt-2 w-full lg:mt-[48rem]   bg-gray-200 h-fit pb-5 pl-5 pt-5">
+    <div className="mt-2 xl:mt-0 w-full lg:mt-[48rem]   bg-gray-200 h-fit pb-5 pl-5 pt-5">
       <div className=" flex-col w-full  sm:grid sm:grid-cols-2 md:grid md:grid-cols-3 lg:grid grid-cols-4 xl:grid grid-cols-4    ">
         <div className=" w-[16rem] md:mt-4">
           <h3 className=" text-2xl font-semibold mb-4">Newsletter</h3>
--- src/select/Shampoo.jsx
+++ src/select/Shampoo.jsx
@@ -4,15 +4,17 @@ import { Link } from "react-router-dom";
 import shamp1 from "./selectImgs/shamp1.jpg";
 import { IoMdArrowDropright } from "react-icons/io";
 import { RiFilter3Fill } from "react-icons/ri";
-import {FaArrowUp } from "react-icons/fa";
+import {FaArrowDown, FaArrowUp } from "react-icons/fa";
 import {MdKeyboardArrowRight } from "react-icons/md";
+import {BsMicrosoft} from 'react-icons/bs'
 import "rsuite/dist/rsuite.min.css";
 import {selectData } from '../select/SelectData'
 import { Dropdown } from "rsuite";
 import General from "../components/General";
+import { AiOutlineMenuUnfold } from "react-icons/ai";
 const Shampoo = () => {
   return (
-    <div className="w-[95%]  mb-10 ml-auto mr-auto">
+    <div className="w-[95%] md:w-[90%] md:mt-0 lg:mt-[0rem]   xl:mt-4  mb-10 ml-auto mr-auto">
       <small className="text-[#8080809f] flex items-center  ">
         {" "}
         <Link className="text-[#000]" to={"/"}>
@@ -26,34 +28,40 @@ const Shampoo = () => {
         Bar Shampoo & Waterless Skincare
       </p>
       <hr className="w-full mt-1 mb-1" />
-      <p className=" mt-0">
+      <p className=" mt-0 md:text-lg">
         Solid skincare and beauty products may be all the rage right now, but
         don't let that trick you into thinking they're trendy and complicated.
         Not at all! If you're ever used bar soap, you won't have any trouble
         embracing other waterless solid formulations, like bar shampoo, or stick
         deodorant.
       </p>
-      <p>
+      <p className=" md:text-lg">
         Waterless and low-water cosmetics come with a number of advantages: they
         feature a higher concentration of active ingredients, they use up fewer
         resources during production, and they generate less waste (as you won't
         have to get rid of plastic packaging when you're done). Are you ready to
         make the switch?
       </p>
-      <div className="flex flex-col items-center">
+      <div className="flex flex-col items-center  md:flex md:flex-row md:justify-between md:mt-10 md:items-start  md:w-full ">
         <div className="mt-10 text-[#000]">
-          <h3 className="mb-2">Waterless Beauty: What Is It & What To Buy.</h3>
-          <p className="text-[#808080d6] font-medium text-base mb-5">
-            Waterless beauty is here to stay! We let you know what waterless
+          <h3 className=" md:hidden mb-2">Waterless Beauty: What Is It & What To Buy.</h3>
+          <h3 className="hidden md:block lg:block xl:hidden md:text-3xl">Waterless Beauty: What Is It &  What To Buy.</h3>
+          <h3 className="hidden md:hidden lg:hidden xl:block  lg:text-4xl lg:mb-3  xl:text-6xl xl:mb-4">Waterless Beauty: What Is It & <br />  What To Buy.</h3>
+          <p className="text-[#808080d6] md:hidden font-medium text-base mb-5">
+            Waterless beauty is here to stay! We let you know  what waterless
             beauty is and also what are the best products.
           </p>
-          <button className="flex items-center mb-8 bg-[gray] text-white py-3 px-3">
+          <p className="hidden md:block md:text-xl text-[#808080d6] font-medium text-base mb-5">
+            Waterless beauty is here to stay! We let you know <br /> what waterless
+            beauty is and also what are the best products.
+          </p>
+          <button className="flex items-center mb-8 bg-[gray] text-white lg:text-xl md:hover:bg-black lg:py-7 md:py-5 md:px-10 lg:px-14 py-3 px-3">
             READ MORE <MdOutlineKeyboardDoubleArrowRight className="ml-2" />
           </button>
         </div>
-        <img src={shamp1} alt="img" />
+        <img src={shamp1} alt="img" className=" md:w-[25rem] md:h-[17] lg:ml-1 lg:w-[32rem] lg:h-[20rem] xl:w-[50rem] xl:h-[28rem]" />
       </div>
-      <div className="filter mt-8 w-[95%] ml-auto mr-auto">
+      <div className="filter mt-8 w-[95%] ml-auto mr-auto sm:hidden">
         <hr className="w-full  mb-4" />
           <p className="flex items-center justify-between w-[50%]">
           <IoMdArrowDropright className=" text-base text-[gray]" />
@@ -65,7 +73,7 @@ const Shampoo = () => {
         
         <hr className="w-full  mt-4" />
       </div>
-      <div className="flex items-center justify-center">
+      <div className="flex items-center justify-center sm:hidden">
       <p>Sort By</p>
          <Dropdown title='Position'    className=' w-[6rem] ml-1 mr-1'>
               <Dropdown.Item>
@@ -79,18 +87,66 @@ const Shampoo = () => {
       </div>
          
       <hr className="w-full " />
-
+       <div className="hidden md:flex md:items-center md:justify-between md:w-full">
+        <div className="flex items-center">
+        <p className="lg:text-base lg:block md:hidden  lg:mr-1 lg:font-semibold">Items 1 to 10 of 95 total | Sort By</p>
+        <p className="md:text-base lg:hidden md:block md:mr-1 md:font-semibold"> Sort By</p>
+         <Dropdown title='Position'    className=' w-[6rem] ml-1 mr-1'>
+              <Dropdown.Item>
+              <Link className='text-[#444] focus:text-[#444] focus:no-underline no-underline ml-0'>Name</Link>
+                </Dropdown.Item>
+              <Dropdown.Item className="hover:bg-[teal]">
+              <Link  className='text-[#444] focus:text-[#444] focus:no-underline no-underline ml-0'>Price</Link>
+                </Dropdown.Item>
+                </Dropdown>
+                <FaArrowDown className=" mr-2 ml-2"/>
+                <p className="text-base font-semibold mr-2">| Show</p>
+                <Dropdown title='1'    className=' w-[3rem] ml-1 mr-1'>
+              <Dropdown.Item>
+              <Link className='text-[#444] focus:text-[#444] focus:no-underline no-underline ml-0'>12</Link>
+                </Dropdown.Item>
+              <Dropdown.Item className="hover:bg-[teal]">
+              <Link  className='text-[#444] focus:text-[#444] focus:no-underline no-underline ml-0'>30</Link>
+                </Dropdown.Item>
+              <Dropdown.Item className="hover:bg-[teal]">
+              <Link  className='text-[#444] focus:text-[#444] focus:no-underline no-underline ml-0'>56</Link>
+                </Dropdown.Item>
+              <Dropdown.Item className="hover:bg-[teal]">
+              <Link  className='text-[#444] focus:text-[#444] focus:no-underline no-underline ml-0'>128</Link>
+                </Dropdown.Item>
+                </Dropdown>
+                <p className="text-base font-semibold ml-2">per page</p>
+        </div>
+         <div className="flex items-center">
+          <p className="mr-3 text-base font-semibold">View as:</p>
+          <p className="mr-3 w-10 h-10 bg-[#8080804c] flex items-center justify-center"> <BsMicrosoft className="text-base"/> </p>
+           <AiOutlineMenuUnfold className="text-xl "/>
+         </div>
+         
+       </div>
+       <hr className="hidden md:block md:w-full" />
+       <div className=" hidden  md:flex md:mt-4 md:items-center md:justify-center">
+          <p className="mr-3 text-base font-semibold">Page:</p>
+          <span className=" cursor-pointer ml-1 mr-0.2 w-10 h-10 flex items-center justify-center bg-[#8080808b]">1</span>
+          <span className=" cursor-pointer ml-1 mr-0.2 w-10 h-10 flex items-center justify-center bg-[#8080802b]">2</span>
+          <span className=" cursor-pointer ml-1 mr-0.2 w-10 h-10 flex items-center justify-center bg-[#8080802b]">3</span>
+          <span className=" cursor-pointer ml-1 mr-0.2 w-10 h-10 flex items-center justify-center bg-[#8080802b]">4</span>
+          <span className=" cursor-pointer ml-1 mr-0.2 w-10 h-10 flex items-center justify-center bg-[#8080802b]">5</span>
+          <span className=" cursor-pointer ml-1 mr-0.2 w-10 h-10 flex items-center justify-center bg-[#8080802b]"><MdKeyboardArrowRight/></span>
+          
+        </div>
+        <hr className="hidden md:block md:w-full" />
       <div  className="main mt-8 ">
-        <div className="card w-[99%] ml-auto pr-8 mr-auto grid grid-cols-2 gap-x-9 gap-y-6">
+        <div className="card w-[99%] ml-auto pr-8  xl:pl-14 lg:pl-11 md:pl-6 md:pr-auto   mr-auto xl:grid xl:grid-cols-5  md:grid md:grid-cols-3 grid grid-cols-2  gap-x-9 gap-y-6">
             {
               selectData.map((data,index)=>{
                 return(
-                  <div className=" box cursor-pointer text-center    w-[11rem] h-[22rem] ">
-                  <img src={data.image} alt="img" />
-                  <p className=" font-medium mt-3">{data.name}</p>
-                  <p className=" font-medium">{data.desc}</p>
-                  <p className="font-semibold">Price: {data.price}</p>
-                  <button className="bg-[#8080805a] py-3 px-4 mt-2">{data.button}</button>
+                  <div className=" box cursor-pointer text-center md:w-[13rem] md:h-[24rem] lg:w-[15rem] lg:h-[27rem]   xl:w-[16rem] xl:h-[32rem]  w-[11rem] h-[22rem] ">
+                  <img src={data.image} alt="img" className="md:w-full lg:w-full xl:w-full"/>
+                  <p className=" font-medium mt-3 xl:text-base lg:text-base">{data.name}</p>
+                  <p className=" font-medium xl:text-base lg:text-base">{data.desc}</p>
+                  <p className="font-semibold xl:text-base lg:text-base">Price: {data.price}</p>
+                  <button className="bg-[#8080805a] py-3 px-4 mt-2 lg:text-base">{data.button}</button>
               
                 </div>
                 )
@@ -98,7 +154,7 @@ const Shampoo = () => {
             }
         </div>
        </div>
-        <div className="flex mt-8 items-center justify-center">
+        <div className=" sm:hidden flex mt-8 items-center justify-center">
           <p>Page:</p>
           <span className=" ml-1 mr-0.2 w-8 h-8 flex items-center justify-center bg-[#8080808b]">1</span>
           <span className=" ml-1 mr-0.2 w-8 h-8 flex items-center justify-center bg-[#8080802b]">2</span>
